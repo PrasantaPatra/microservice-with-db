e553fd76d565b96a9c378c7d11d76f07579c676a
prasanta <prasanta.prema@gmail.com>
2019-10-28 20:56:19 +0530

    mongodb complete

---
+++ mongodbdemo/src/main/java/com/mongodb/CustomerController.java
@@ -0,0 +1,42 @@
+package com.mongodb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @PostMapping("/addCustomer")
+    public void save(@RequestBody Customer customer) {
+        repository.save(customer);
+    }
+
+    @GetMapping("/findAll")
+    public List<Customer> findAll() {
+        List<Customer> list = new ArrayList<>();
+        list = repository.findAll();
+        return list;
+    }
+
+    @GetMapping("/getby/{firstname}")
+    public Customer findByFirstName(@PathVariable String firstname) {
+        return repository.findByFirstName(firstname);
+    }
+
+    @PutMapping("/updateCustomer")
+    public void update(@RequestBody Customer customer) {
+        repository.save(customer);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable String id) {
+        repository.deleteById(id);
+    }
+
+}
---
+++ mongodbdemo/src/main/java/com/mongodb/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<Customer,String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+}
